4124144cd4ca3661a9f934289bdd4a9ea3f614fe
MinhDucIT43 <nmduc.ctuk43@gmail.com>
2025-01-04 18:32:01 +0700

    Update 04/01/2025 - 18h:31

--- app/Http/Requests/StaffRequest.php
+++ app/Http/Requests/StaffRequest.php
@@ -23,21 +23,33 @@ class StaffRequest extends FormRequest
     public function rules(): array
     {
         $staff_code = $this->route('staff_code');
-        $rules = [
-            'fullName' => 'required',
-            'imgOfStaff' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
-            'birthday' => 'required|before:-18 years',
-            'address' => 'required',
-            'workingDay' => 'required',
-            'phone' => ['required','numeric'],
-            'position' => 'required',
-            //'password' => 'required'
-        ];
-
-        if($staff_code){
-            $rules['phone'][] = Rule::unique('staffs','phone')->ignore($staff_code,'staff_code');
-        }else{
-            $rules['phone'][] = Rule::unique('staffs','phone');
+        if($this->input('formType') == 'addStaffType'){
+            $rules = [
+                'fullName' => 'required',
+                'imgOfStaff' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+                'birthday' => 'required|before:-18 years',
+                'address' => 'required',
+                'workingDay' => 'required',
+                'phone' => 'required|numeric|unique:staffs,phone',
+                'position' => 'required',
+                'password' => 'required'
+            ];
+        }
+        if($this->input('formType') == 'updateStaffType'){
+            $rules = [
+                'fullName' => 'required',
+                'imgOfStaff' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+                'birthday' => 'required|before:-18 years',
+                'address' => 'required',
+                'workingDay' => 'required',
+                'phone' => ['required','numeric'],
+                'position' => 'required',
+            ];
+            if($staff_code){
+                $rules['phone'][] = Rule::unique('staffs','phone')->ignore($staff_code,'staff_code');
+            }else{
+                $rules['phone'][] = Rule::unique('staffs','phone');
+            }
         }
 
         return $rules;
@@ -47,7 +59,7 @@ class StaffRequest extends FormRequest
     {
         return [
             'fullName.required' => "Vui lòng nhập tên nhân viên.",
-            //'imgOfStaff.required' => "Vui lòng chọn ảnh nhân viên.",
+            'imgOfStaff.required' => "Vui lòng chọn ảnh nhân viên.",
             'imgOfStaff.image' => "Vui lòng chọn file ảnh.",
             'imgOfStaff.mimes' => "Chỉ chấp nhận tệp có định dạng .jpg hoặc .png",
             'imgOfStaff.max' => "Kích thước tệp không được vượt quá 5MB.",
@@ -59,7 +71,7 @@ class StaffRequest extends FormRequest
             'phone.numeric' => "Số điện thoại chỉ được nhập số.",
             'phone.unique' => "Đã có nhân viên có số điện thoại này.",
             'position.required' => "Vui lòng chọn chức vụ.",
-            //'password.required' => "Vui lòng nhập mật khẩu.",
+            'password.required' => "Vui lòng nhập mật khẩu.",
         ];
     }
 }
