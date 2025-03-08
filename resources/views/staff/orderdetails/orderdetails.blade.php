4ce97f15e9d58d0fd56374d78aafe416238cf635
MinhDucIT43 <nmduc.ctuk43@gmail.com>
2025-03-08 17:08:16 +0700

    Update 08/03/2025 - 17h:09

--- resources/views/staff/orderdetails/orderdetails.blade.php
+++ resources/views/staff/orderdetails/orderdetails.blade.php
@@ -6,15 +6,45 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$table->nameTable}}</title>
     <link rel="shortcut icon" href="{{asset('resources/images/login/logo.png')}}">
-    <link rel="stylesheet" href="{{asset('resources/css/staff/staff.css')}}">
+    <link rel="stylesheet" href="{{asset('resources/css/staff/orderdetails/orderdetails.css')}}">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <!-- Link Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Link FontAwesome -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 </head>
 <body>
-    <h1>Đã chọn {{$quantity}} vé @foreach($SelectedTicketPrice as $Selected) {{$Selected->nameTicket}} với giá {{$Selected->price}} @endforeach</h1>
+    <div class="all">
+        <div class="header">    
+            <a href="{{ url()->previous() }}" type="button" class="btn" style="color:blue" role="button"><i class="fas fa-arrow-alt-left fa-lg return" style="font-size: 180%"></i></a>
+            <div id="clock"></div>
+            <strong id="inf-staff">{{ auth()->guard('staffs')->user()->fullName }}</strong>
+        </div>
+        <div class="contents">
+            <div class="selected">
+                <h1>Món đã chọn</h1>
+            </div>
+            <div class="menus">
+                <div class="categorys">
+                    <ul class="nav nav-pills">
+                        <li class="nav-item">
+                          <a class="nav-link active" href="#">Thức ăn</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">Nước uống</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">Vé buffet</a>
+                        </li>
+                      </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="{{asset('resources/js/master/clock.js')}}"></script>
 </body>
 </html>
